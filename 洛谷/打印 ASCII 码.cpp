bbd00ed44e5ca27422caf433eb022cccfbbca84c
宁萌摹 <428337480@qq.com>
2023-07-12 17:37:59 +0800

    第2

---
+++ 洛谷/打印 ASCII 码.cpp
@@ -0,0 +1,14 @@
+//打印ASCII码
+#include<iostream>
+int main()
+{
+//     char ch;
+//     scanf("%c",&ch);
+//     printf("%d\n",ch);
+//     return 0;
+     int ch;
+    scanf("%d",&ch);
+    printf("%c\n",ch);
+    return 0;
+}
+//打印字符
---
+++ 洛谷/浮点数向零舍入.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+int main()
+{
+    double num;
+    std::cin>>num;
+    printf("%ld\n",(long long)num);
+    
+}
